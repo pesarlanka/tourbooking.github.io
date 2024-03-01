5aa8c14837675b324c8a04261031831d489b9d75
Pesarlanka devendrababu <92357125+pesarlanka@users.noreply.github.com>
2024-03-01 18:23:49 +0530

    routes

---
+++ Tour-management/backend/routes/users.js
@@ -0,0 +1,13 @@
+
+import express from 'express'
+import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/useController.js'
+
+const router = express.Router()
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js'
+
+router.put('/:id',verifyUser, updateUser)
+router.delete('/:id',verifyUser, deleteUser)
+router.get('/:id',verifyUser, getSingleUser)
+router.get('/',verifyAdmin, getAllUser)
+
+export default router;
